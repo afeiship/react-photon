064eaa092d411ae8e98b5b0baa0f8505921b6f59
afeiship <1290657123@qq.com>
2019-12-21 22:32:06 +0800

    docs: update main hash

--- build/webpack.docs.conf.js
+++ build/webpack.docs.conf.js
@@ -3,14 +3,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const baseConfig = require('./webpack.base.conf');
 const OfflinePlugin = require('offline-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = (inEnv) => {
   return merge(baseConfig(inEnv), {
     mode: 'production',
     output: {
-      path: path.resolve(__dirname, '..', 'docs')
+      path: path.resolve(__dirname, '..', 'docs'),
+      filename: '[name].[hash:6].js'
     },
     plugins: [
+      new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         template: './public/index.ejs'
       }),
