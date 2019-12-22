6d8334d63280d1462a0bda4729fd5e12411e1d50
afeiship <1290657123@qq.com>
2019-12-22 16:38:41 +0800

    add: sw fallback.js

--- build/webpack.docs.conf.js
+++ build/webpack.docs.conf.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const baseConfig = require('./webpack.base.conf');
 const OfflinePlugin = require('offline-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = (inEnv) => {
   return merge(baseConfig(inEnv), {
     mode: 'production',
@@ -15,6 +15,12 @@ module.exports = (inEnv) => {
     },
     plugins: [
       new CleanWebpackPlugin(),
+      new webpack.EnvironmentPlugin({
+        NODE_ENV: 'production'
+      }),
+      new CopyWebpackPlugin([
+        { from: './public/assets/fallback.js', to: './fallback.js' }
+      ]),
       new HtmlWebpackPlugin({
         template: './public/index.ejs'
       }),
---
+++ public/assets/fallback.js
@@ -0,0 +1,4 @@
+console.warn('You can instead of this file to server url.');
+console.warn('DO NOT DELETE THIS FILE!!!');
+
+window.__SW_DISABLED__ = false;
