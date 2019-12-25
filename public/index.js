d00af5f727afbb9936f9fa75257ad63857fc7498
afeiship <1290657123@qq.com>
2019-12-26 00:48:24 +0800

    fix: reset style

--- public/index.js
+++ public/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import App from './app';
 
 // styles:
+import "@feizheng/webkit-sass-reset/dist/base.scss";
 import '@feizheng/photon';
 import '@feizheng/react-sw-update-tips/dist/style.scss';
 import 'highlight.js/styles/github.css';
