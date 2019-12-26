253ea242fc958e338aa683dd6910df9dd6e7f4d2
afeiship <1290657123@qq.com>
2019-12-27 00:24:06 +0800

    docs: update nav.json

--- public/app.js
+++ public/app.js
@@ -16,34 +16,10 @@ import ReactClipboard from '@feizheng/react-clipboard';
 import ReactFullImage from '@feizheng/react-full-image';
 
 const routes = require('root/public/assets/routes.json');
+const navs = require('root/public/assets/navs.json');
 
 export default class extends React.Component {
-  state = {
-    hasUpdate: false,
-    navs: [
-      { type: 'title', label: 'Preview' },
-      { type: 'nav-link', icon: 'search', label: 'all', to: '/all' },
-      { type: 'title', label: 'Components' },
-      { type: 'nav-link', icon: 'note-beamed', label: 'button', to: '/button' },
-      { type: 'nav-link', icon: 'music', label: 'button-group', to: '/button-group' },
-      { type: 'nav-link', icon: 'search', label: 'checkbox', to: '/checkbox' },
-      { type: 'nav-link', icon: 'flashlight', label: 'form-group', to: '/form-group' },
-      { type: 'nav-link', icon: 'mail', label: 'icon', to: '/icon' },
-      { type: 'nav-link', icon: 'heart', label: 'input', to: '/input' },
-      { type: 'nav-link', icon: 'star', label: 'list-group', to: '/list-group' },
-      { type: 'nav-link', icon: 'user', label: 'nav-group', to: '/nav-group' },
-      { type: 'nav-link', icon: 'video', label: 'select', to: '/select' },
-      { type: 'nav-link', icon: 'camera', label: 'table', to: '/table' },
-      { type: 'nav-link', icon: 'layout', label: 'tabs', to: '/tabs' },
-      { type: 'nav-link', icon: 'check', label: 'textarea', to: '/textarea' },
-      { type: 'nav-link', icon: 'help', label: 'toolbar', to: '/toolbar' },
-      { type: 'nav-link', icon: 'quote', label: 'window', to: '/window' }
-    ]
-  };
-
-  get component() {
-    return location.hash.slice(2);
-  }
+  state = { hasUpdate: false, navs };
 
   componentDidMount() {
     NxOfflineSw.install({
@@ -54,7 +30,7 @@ export default class extends React.Component {
   }
 
   render() {
-    const { navs, hasUpdate } = this.state;
+    const { hasUpdate } = this.state;
     return (
       <div className="example-component-window">
         <ReactFullImage src="http://photonkit.com/assets/img/photo-couch.png" />
