b12f893a61f743f9f21365e2b9e167000b4d1f08
afeiship <1290657123@qq.com>
2019-12-29 17:40:53 +0800

    docs: update values

--- build/raw-snippets.js
+++ build/raw-snippets.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 const glob = require('glob');
 
 const files = glob.sync('./public/components/**/index.js');
+const excludes = ['installation'];
 
 // generate
 files.forEach((item) => {
-  fs.writeFileSync(`${item}.snippet`, fs.readFileSync(item));
+  if (!excludes.includes(item)) {
+    fs.writeFileSync(`${item}.snippet`, fs.readFileSync(item));
+  }
 });
--- public/components/github/index.js
+++ public/components/github/index.js
@@ -2,10 +2,25 @@ import Property from 'root/public/components/layout/property';
 import Container from 'root/public/components/layout/container';
 import ReactHighlight from '@feizheng/react-highlight';
 import ReactClipboard from '@feizheng/react-clipboard';
+import Icon from 'packages/icon/main';
 
 export default () => {
   return (
     <Container>
+      <div className="padded-vertically">
+        <p>
+          <a target="_blank" href="https://github.com/afeiship/react-photon">
+            <Icon value="link" />
+            <span>react-photon</span>
+          </a>
+        </p>
+        <p>
+          <a target="_blank" href="http://photonkit.com/">
+            <Icon value="link" />
+            <span>photonkit</span>
+          </a>
+        </p>
+      </div>
     </Container>
   );
 };
